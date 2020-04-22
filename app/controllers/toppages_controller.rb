46ca0cc65c750524d636fd7a8c60b527ba23962d
umekinomori <umeki7538@icloud.com>
2020-04-23 01:24:04 +0900

    toppage action insdex
    ルーディングの設定修正
    toppagecontrollerのアクションを設定

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,2 +1,6 @@
 class ToppagesController < ApplicationController
+  def index
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "groups#index"
+  root 'toppages#index'
+
 end
