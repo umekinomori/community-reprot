1567559b590bd9127dd2591dcec8bf206187dced
umekinomori <umeki7538@icloud.com>
2020-05-01 03:10:07 +0900

    commit

--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,4 +1,4 @@
-App.relationship = App.cable.subscriptions.create("RelationshipsChannel", {
+App.room = App.cable.subscriptions.create("RoomChannel", {
   connected: function() {
     console.log('connected')
     // Called when the subscription is ready for use on the server
@@ -19,12 +19,13 @@ App.relationship = App.cable.subscriptions.create("RelationshipsChannel", {
   }
 });
 
+
 document.addEventListener('DOMContentLoaded', function(){
   const input = document.getElementById('chat-input')
   const button = document.getElementById('button')
   button.addEventListener('click', function(){
     const content = input.value
-    App.relationship.speak(content)
+    App.room.speak(content)
     input.value = ''
   })
 })
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  
   devise_for :users
   root to: 'relationships#show'
-  resources :relationships, only: [:create, :destroy, :show]
+  resources :relationships, only: [:create, :destroy]
 
   mount ActionCable.server => '/cable'
 end
