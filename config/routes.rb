52228e20cda08c4f0333a7edbd375293f6087b0e
umekinomori <umeki7538@icloud.com>
2020-05-01 11:07:08 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'relationships#show'
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy, :show]
 
   mount ActionCable.server => '/cable'
 end
